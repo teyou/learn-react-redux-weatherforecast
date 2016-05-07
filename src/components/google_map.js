f147cbb7dcef7d3089dc6e8e5026a88fd087df4e
Rudy <pudding.pearl.tea@gmail.com>
2016-05-08 01:08:23 +0800

    lesson 65 - add google map

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -5,6 +5,12 @@ export default (props) => {
   return(
     <GoogleMapLoader
       containerElement={ <div style={{height: '100%'}} /> }
+      googleMapElement={
+        <GoogleMap
+          defaultZoom={12}
+          defaultCenter={{lat: props.lat, lng: props.lon}}
+        />
+      }
      />
   );
 }
