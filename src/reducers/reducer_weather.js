161fd5a3841c4366032aa319f303e3de12417164
Rudy <pudding.pearl.tea@gmail.com>
2016-05-07 22:06:58 +0800

    lesson 58

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,13 @@
-export default function(state = null, action){
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action){
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //return state.concat([action.payload.data]); //es5
+      return [ action.payload.data, ...state]; //es6
+  }
   console.log('Action received', action);
-  
+
   return state;
 }
